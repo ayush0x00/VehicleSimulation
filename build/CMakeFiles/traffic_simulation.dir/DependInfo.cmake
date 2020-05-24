f08275b88a6e0996956378fad76624c5514a01c1
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-05-25 02:20:03 +0530

    Implemented traffic light

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cipher/Desktop/VehicleSimulation/src/Graphics.cpp" "/home/cipher/Desktop/VehicleSimulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/home/cipher/Desktop/VehicleSimulation/src/Intersection.cpp" "/home/cipher/Desktop/VehicleSimulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/home/cipher/Desktop/VehicleSimulation/src/Street.cpp" "/home/cipher/Desktop/VehicleSimulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/home/cipher/Desktop/VehicleSimulation/src/TrafficLight.cpp" "/home/cipher/Desktop/VehicleSimulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/home/cipher/Desktop/VehicleSimulation/src/TrafficObject.cpp" "/home/cipher/Desktop/VehicleSimulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/home/cipher/Desktop/VehicleSimulation/src/TrafficSimulator-Final.cpp" "/home/cipher/Desktop/VehicleSimulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/home/cipher/Desktop/VehicleSimulation/src/Vehicle.cpp" "/home/cipher/Desktop/VehicleSimulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
